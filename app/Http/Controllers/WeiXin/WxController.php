27f97eab1b19af9f330db1e5618b2e51e4952a33
root <2111255898@qq.com>
2019-12-07 03:22:28 +0000

    微信接入

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -9,6 +9,26 @@ class WxController extends Controller
 {
     public function wechat()
     {
+        $toke = '2259b56f5898cd6192c50d338723d9e4';
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
 
+        if( $tmpStr == $signature ){
+            echo $echostr;
+        }else{
+            die("not ok");
+        }
     }
+
+    public function index(Request $request)
+    {
+        $echostr=$request->input('echostr');
+        echo $echostr;die;
+    }
+
 }
