1a3b564fba71ca83d219f1db5628c9a7e70f0a43
root <2111255898@qq.com>
2019-12-19 11:42:59 +0800

    投票头像

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -4,6 +4,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 class VoteController extends Controller
 {
+    /**
+     * 测试使用，线上禁用
+     */
+    public function delKey()
+    {
+        $key = $_GET['k'];
+        echo 'Delete Key: '.$key;echo '</br>';
+        Redis::del($key);
+    }
     public function index()
     {
         //echo '<pre>';print_r($_GET);echo '</pre>';
@@ -12,21 +21,25 @@ class VoteController extends Controller
         $data = $this->getAccessToken($code);
         //获取用户信息
         $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
+        //保存用户信息
+        $userinfo_key = 'h:u:'.$data['openid'];
+        Redis::hMset($userinfo_key,$user_info);
         // 处理业务逻辑
         $openid = $user_info['openid'];
         $key = 'ss:vote:zhangsan';
         //判断是否已经投过票
         if(Redis::zrank($key,$user_info['openid'])){
-            echo "已经投过票了";
+            echo "已经投过票了";echo '</br>';
         }else{
             Redis::Zadd($key,time(),$openid);
         }
         $total = Redis::zCard($key);        // 获取总数
-        echo '投票总人数： '.$total;echo '</br>';
         $members = Redis::zRange($key,0,-1,true);       // 获取所有投票人的openid
-        echo '<pre>';print_r($members);echo '</pre>';
         foreach($members as $k=>$v){
-            echo "用户： ".$k . ' 投票时间: '. date('Y-m-d H:i:s',$v);echo '</br>';
+            $u_k = 'h:u:'.$k;
+            $u = Redis::hgetAll($u_k);
+            //$u = Redis::hMget($u_k,['openid','nickname','sex','headimgurl']);
+            echo ' <img src="'.$u['headimgurl'].'"> ';
         }
     }
     /**
@@ -54,5 +67,22 @@ class VoteController extends Controller
             die("出错了 40001");       // 40001 标识获取用户信息失败
         }
         return $data;           // 返回用户信息
+    }
+    public function hashTest()
+    {
+        $uid = 1000;
+        $key = 'h:user_info:uid:'.$uid;
+        $user_info = [
+            'uid'       => $uid,
+            'user_name'	=> 'zhangsan',
+            'email'		=> 'zhangsan@qq.com',
+            'age'		=> 22,
+            'sex'		=> 1
+        ];
+        Redis::hMset($key,$user_info);
+        die;
+        echo '<hr>';
+        $u = Redis::hGetAll($key);
+        echo '<pre>';print_r($u);echo '</pre>';
     }
 }
