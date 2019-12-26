23a663ce264b165f71782f66fdb8a6db2fff1775
root <2111255898@qq.com>
2019-12-26 12:00:03 +0800

    kc

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -118,23 +118,33 @@ class WxController extends Controller
                 }
             }elseif($event=='CLICK'){           // 菜单点击事件
                 if($xml_obj->EventKey=='weather'){
-                    //如果是 获取天气
-                    //请求第三方接口 获取天气
-                    $weather_api = 'https://free-api.heweather.net/s6/weather/now?location=beijing&key=d957029d5931428f8eef6ba241aefdd7';
-                    $weather_info = file_get_contents($weather_api);
-                    $weather_info_arr = json_decode($weather_info,true);
-                    $cond_txt = $weather_info_arr['HeWeather6'][0]['now']['cond_txt'];
-                    $tmp = $weather_info_arr['HeWeather6'][0]['now']['tmp'];
-                    $wind_dir = $weather_info_arr['HeWeather6'][0]['now']['wind_dir'];
-                    $msg = $cond_txt . ' 温度： '.$tmp . ' 风向： '. $wind_dir;
-                    $response_xml = '<xml>
-  <ToUserName><![CDATA['.$openid.']]></ToUserName>
-  <FromUserName><![CDATA['.$xml_obj->ToUserName.']]></FromUserName>
-  <CreateTime>'.time().'</CreateTime>
-  <MsgType><![CDATA[text]]></MsgType>
-  <Content><![CDATA['. date('Y-m-d H:i:s') .  $msg .']]></Content>
+                    if ($xml_obj->Event=='CLICK') {
+//判断并且触发
+                            $ke=Ke::where('openid','=',$openid)->first();
+                            if($ke){
+                                $b='第一节课'.$ke['ka']."\n".'第二节课'.$ke['kb']."\n".'第三节课'.$ke['kc']."\n".'第四节课'.$ke['kd'];
+                                $a='<xml>
+<ToUserName><![CDATA['.$openid.']]></ToUserName>
+<FromUserName><![CDATA['.$touser.']]></FromUserName>
+<CreateTime>'.$time.'</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA['.$b.']]></Content>
 </xml>';
-                    echo $response_xml;
+                                echo $a;
+                            }else{
+                                $b='您还没添加课程，请赶快去课程管理添加把';
+                                $a='<xml>
+<ToUserName><![CDATA['.$openid.']]></ToUserName>
+<FromUserName><![CDATA['.$touser.']]></FromUserName>
+<CreateTime>'.$time.'</CreateTime>
+<MsgType><![CDATA[text]]></MsgType>
+<Content><![CDATA['.$b.']]></Content>
+</xml>';
+                                echo $a;
+                            }
+
+                    }
+
                 }
             }
             // 判断消息类型
