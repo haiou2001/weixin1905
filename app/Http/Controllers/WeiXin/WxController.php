07e85aa2ccfa7af6405b64b26b2ef005b3b07f07
root <2111255898@qq.com>
2019-12-26 09:28:12 +0800

    kc

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -266,7 +266,7 @@ class WxController extends Controller
          */
         public function createMenu()
         {
-            $urls ='http://wjk.xx20.top/vote';
+            $urls ='http://wjk.xx20.top/course/index';
             $urls2 ='http://wjk.xx20.top/';
             $redirect_uri = urlencode($urls);        //授权后跳转页面
             $redirect_uri2 = urlencode($urls2);        //授权后跳转页面
