55063e72d12fb35c5f0fcc84facf379358cb7a6f
root <2111255898@qq.com>
2019-12-26 09:55:03 +0800

    kc

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -1,13 +1,60 @@
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <title>课程</title>
+    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-1231313
+
+<form class="form-horizontal" role="form">
+    <div class="form-group">
+        <label for="firstname" class="col-sm-2 control-label">第一节课:</label>
+        <div class="col-sm-10">
+            <select name="" class="form-control" id="firstname"
+                    placeholder="请输入名字">
+                <option value="">PHP</option>
+                <option value="">JS</option>
+                <option value="">JAVA</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="firstname" class="col-sm-2 control-label">第二节课:</label>
+        <div class="col-sm-10">
+            <select name="" class="form-control" id="firstname"
+                    placeholder="请输入名字">
+                <option value="">语文</option>
+                <option value="">历史</option>
+                <option value="">生物</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="firstname" class="col-sm-2 control-label">第三节课:</label>
+        <div class="col-sm-10">
+            <select name="" class="form-control" id="firstname"
+                    placeholder="请输入名字">
+                <option value="">数学</option>
+                <option value="">科学</option>
+                <option value="">物理</option>
+            </select>
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="firstname" class="col-sm-2 control-label">第四节课:</label>
+        <div class="col-sm-10">
+            <select name="" class="form-control" id="firstname"
+                    placeholder="请输入名字">
+                <option value="">英语</option>
+                <option value="">汉语</option>
+                <option value="">舞蹈</option>
+            </select>
+        </div>
+    </div>
+    <center><button type="submit"  class="btn btn-default">提交</button></center>
+</form>
 </body>
 </html>
