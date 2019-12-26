328f8716230a93054b9ef64f1d07382022f1ff5b
root <2111255898@qq.com>
2019-12-26 11:46:25 +0800

    kc

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -132,6 +132,7 @@ class WxController extends Controller
   <FromUserName><![CDATA['.$xml_obj->ToUserName.']]></FromUserName>
   <CreateTime>'.time().'</CreateTime>
   <MsgType><![CDATA[text]]></MsgType>
+  <Content><![CDATA['. date('Y-m-d H:i:s') .  $msg .']]></Content>
 </xml>';
                     echo $response_xml;
                 }
